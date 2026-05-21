9b951d37d52412e81daaa33f26376bfa9ad9fbe2
BillyVNYT <billy.gaming2012@gmail.com>
2026-05-21 22:53:12 +0700

    Update dependencies and enhance game logic with winning animations

--- plvn/src/App.jsx
+++ plvn/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
@@ -6,6 +6,24 @@ function App() {
   const [p2, setP2] = useState(10)
   const [p3, setP3] = useState(10)
   const [p4, setP4] = useState(10)
+
+  const [win, setWin] = useState("");
+
+  var sumTeam1 = p1+p2;
+  var sumTeam2 = p3+p4;
+  useEffect(() => {
+    if(sumTeam1 < sumTeam2){
+      setWin("team2better");
+      console.log("team2");
+    } else if(sumTeam1 > sumTeam2){
+      setWin("team1better");
+      console.log("team1");
+    } else {
+      setWin("");
+      console.log("none");
+    }
+  }), [sumTeam1, sumTeam2];
+
   return (
     <>
       <input type="number" value={p1}
@@ -19,8 +37,11 @@ function App() {
       <input type="number" value={p4}
         onChange={(e) => setP4(Number(e.target.value))}
       />
-      <div id="container">
-        <div id="container1">
+      <div id='line'></div>
+      <div id="container" className={win}>
+        <div id="container1" style={{
+          transform: `translateY(-${Math.max(p1, p2) * 2.25}px)`
+        }}>
             <div class="circle" style={
               {
                 width: `${p1*10}px`,
@@ -36,7 +57,9 @@ function App() {
               }
             }></div>
         </div>
-        <div id="container2">
+        <div id="container2" style={{
+          transform: `translateY(-${Math.max(p1, p2) * 2.25}px)`
+        }}>
             <div class="circle" style={
               {
                 width: `${p3*10}px`,
